eb50b249b4dd8f6db6c5db38c2f394c5db9bf8bc
Luka <luka.prelicads@gmail.com>
2020-05-08 22:03:26 +0100

    formatting

--- static/js/js.js
+++ static/js/js.js
@@ -4,7 +4,6 @@ function runcalc() {
     for (const pair of new FormData(document.getElementById("runCalcForm"))) {
         data.append(pair[0], pair[1]);
     }
-
     fetch(url, {
         method: 'post',
         body: data,
@@ -24,37 +23,30 @@ function runcalc() {
         console.log(r);
         console.log(r.result);
         return;
-
     });
 
 }
 function updateeq(event) {
     var formElements = Array.from(document.getElementById("runCalcForm").elements);
     formElements.forEach(val=> {
-        console.log(val.id,val.value);
         input = document.getElementById(val.id+"eq");
         if(input !=null)
             input.innerHTML = val.value;
         if(val.id.includes("Approximate")){
                     input.innerHTML = "("+val.id[0]+") ~"+val.value;
         }
-
         if(val.id.includes("xExchangeRate")){
-            console.log(val.id)
              document.getElementById(val.id+"eqAprox").innerHTML = "("+val.id[0]+") ~"+val.value;
              document.getElementById(val.id+"eq1").innerHTML = val.value;
              document.getElementById(val.id+"eq2").innerHTML = val.value;
              }
-
     });
-    console.log(formElements);
 }
 window.addEventListener('load', function() {
     updateeq(event);
     console.log('All assets are loaded')
-    console.log(document.querySelectorAll('#runCalcForm input'))
 document.querySelectorAll('#runCalcForm input').forEach(x =>
     x.addEventListener('change', (event) => {
-      updateeq(event);
+    updateeq(event);
 }));
 })
